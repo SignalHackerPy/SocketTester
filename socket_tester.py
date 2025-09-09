4bd680cbd8209f8829e91204be1c9a310fd81207
SignalHackerPy <heha5@proton.me>
2025-09-09 16:25:45 +0200

    add IPv6 support

--- socket_tester.py
+++ socket_tester.py
@@ -3,6 +3,10 @@ import socket
 from psutil import net_if_addrs
 from argparse import ArgumentParser, Namespace
 
+_family_type = {
+    4: socket.AF_INET,
+    6: socket.AF_INET6
+}
 _socket_type = {
     'TCP': socket.SOCK_STREAM,
     'UDP': socket.SOCK_DGRAM
@@ -10,10 +14,10 @@ _socket_type = {
 
 
 class CustomSocket:
-    def __init__(self, mode: str, source_ip_address: str, source_port: int):
-        self.s = socket.socket(socket.AF_INET, _socket_type.get(mode))
+    def __init__(self, family_type: int, socket_type: str, addr):
+        self.s = socket.socket(_family_type.get(family_type), _socket_type.get(socket_type.upper()))
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.s.bind((source_ip_address, source_port))
+        self.s.bind(addr)
 
     def print_source(self):
         print('Source IP: ', self.s.getsockname()[0])
@@ -21,8 +25,8 @@ class CustomSocket:
 
 
 class TCPSocketServer(CustomSocket):
-    def __init__(self, source_ip_address: str, source_port: int):
-        super().__init__('TCP', source_ip_address, source_port)
+    def __init__(self, family_type: int, addr):
+        super().__init__(family_type, 'TCP', addr)
 
     def listen(self):
         self.s.listen()
@@ -32,8 +36,8 @@ class TCPSocketServer(CustomSocket):
 
 
 class TCPSocketClient(CustomSocket):
-    def __init__(self, source_ip_address: str, source_port: int):
-        super().__init__('TCP', source_ip_address, source_port)
+    def __init__(self, family_type: int, addr):
+        super().__init__(family_type, 'TCP', addr)
 
     def connect(self, address):
         self.s.connect(address)
@@ -43,8 +47,8 @@ class TCPSocketClient(CustomSocket):
 
 
 class UDPSocket(CustomSocket):
-    def __init__(self, source_ip_address: str, source_port: int):
-        super().__init__('UDP', source_ip_address, source_port)
+    def __init__(self, family_type: int, addr):
+        super().__init__(family_type, 'UDP', addr)
 
     def sendto(self, data, address):
         self.s.sendto(data, address)
@@ -54,10 +58,11 @@ class UDPSocket(CustomSocket):
 
 
 class NetInterface:
-    def __init__(self, name, ip, mask):
+    def __init__(self, name, ip, mask, af):
         self.name = name
         self.ip = ip
         self.mask = mask
+        self.af = af
 
 
 class InterfaceInfo:
@@ -65,11 +70,11 @@ class InterfaceInfo:
         self.interfaces = []
         for interface, addrs in net_if_addrs().items():
             for addr in addrs:
-                if addr.family == socket.AF_INET:
-                    self.interfaces.append(NetInterface(interface, addr.address, addr.netmask))
+                if addr.family == socket.AF_INET or addr.family == socket.AF_INET6:
+                    self.interfaces.append(NetInterface(interface, addr.address, addr.netmask, addr.family))
 
 
-def separator(n: int = 20, c: str = '-'):
+def separator(n: int = 50, c: str = '-'):
     print(c * n)
 
 
@@ -83,8 +88,23 @@ def list_interfaces(args):
         separator()
 
 
-def print_banner(mode: str):
-    print(f'{mode} Socket Testing\n')
+def get_source_ip_info(socket_type: str, family_type: int, source_ip_address: str, source_port: int):
+    if not source_ip_address:
+        interfaces = InterfaceInfo()
+        int_ip = ''
+        for interface in interfaces.interfaces:
+            if interface.name != 'lo' and (interface.af == _family_type.get(family_type)):
+                int_ip = interface.ip
+                break
+        return socket.getaddrinfo(int_ip, source_port, _family_type.get(family_type),
+                                  _socket_type.get(socket_type))[0][4]
+
+    return socket.getaddrinfo(source_ip_address, source_port, _family_type.get(family_type),
+                              _socket_type.get(socket_type))[0][4]
+
+
+def print_banner(socket_type: str):
+    print(f'{socket_type} Socket Testing\n')
 
 
 def print_destination(address):
@@ -96,25 +116,25 @@ def print_waiting():
     print('Waiting for connection...\n')
 
 
-def print_info(s: CustomSocket, mode: str):
-    print_banner(mode)
+def print_info(s: CustomSocket, socket_type: str):
+    print_banner(socket_type)
     s.print_source()
     print_waiting()
 
 
 def server_run(args):
-    if args.mode.upper() == 'TCP':
-        s = TCPSocketServer(args.source_ip_address, args.source_port)
+    if args.socket_type.upper() == 'TCP':
+        s = TCPSocketServer(args.family_type, args.source_addr)
         s.listen()
 
-        print_info(s, args.mode.upper())
+        print_info(s, args.socket_type.upper())
 
         conn, addr = s.accept()
         print_destination(addr)
     else:
-        s = UDPSocket(args.source_ip_address, args.source_port)
+        s = UDPSocket(args.family_type, args.source_addr)
 
-        print_info(s, args.mode.upper())
+        print_info(s, args.socket_type.upper())
 
         data, addr = s.recvfrom()
         print_destination(addr)
@@ -122,17 +142,17 @@ def server_run(args):
 
 
 def client_run(args):
-    if args.mode.upper() == 'TCP':
-        s = TCPSocketClient(args.source_ip_address, args.source_port)
+    if args.socket_type.upper() == 'TCP':
+        s = TCPSocketClient(args.family_type, args.source_addr)
 
-        print_info(s, args.mode.upper())
+        print_info(s, args.socket_type.upper())
 
         s.connect((args.destination, args.destination_port))
         print_destination(s.getpeername())
     else:
-        s = UDPSocket(args.source_ip_address, args.source_port)
+        s = UDPSocket(args.family_type, args.source_addr)
 
-        print_info(s, args.mode.upper())
+        print_info(s, args.socket_type.upper())
 
         s.sendto(b'0', (args.destination, args.destination_port))
         data, addr = s.recvfrom()
@@ -154,7 +174,6 @@ def parse_args() -> Namespace:
     server_parser.add_argument(
         '-I', '--source-ip-address',
         metavar='IP',
-        default=socket.gethostbyname(socket.gethostname())
     )
     server_parser.add_argument(
         '-P', '--source-port',
@@ -163,9 +182,15 @@ def parse_args() -> Namespace:
         default=0
     )
     server_parser.add_argument(
-        '-m', '--mode',
+        '-s', '--socket-type',
         choices=['tcp', 'udp'],
         default='tcp'
+    )
+    server_parser.add_argument(
+        '-t', '--family-type',
+        choices=[4, 6],
+        default=4,
+        type=int,
     )
 
     # PROGRAM client
@@ -175,7 +200,6 @@ def parse_args() -> Namespace:
     client_parser.add_argument(
         '-I', '--source-ip-address',
         metavar='IP',
-        default=socket.gethostbyname(socket.gethostname())
     )
     client_parser.add_argument(
         '-P', '--source-port',
@@ -195,9 +219,15 @@ def parse_args() -> Namespace:
         required=True
     )
     client_parser.add_argument(
-        '-m', '--mode',
+        '-s', '--socket-type',
         choices=['tcp', 'udp'],
         default='tcp'
+    )
+    client_parser.add_argument(
+        '-t', '--family-type',
+        type=int,
+        choices=[4, 6],
+        default=4
     )
 
     return parser.parse_args()
@@ -205,6 +235,9 @@ def parse_args() -> Namespace:
 
 def main():
     args = parse_args()
+
+    args.source_addr = get_source_ip_info(args.socket_type.upper(), args.family_type, args.source_ip_address,
+                                          args.source_port)
 
     if hasattr(args, 'func'):
         args.func(args)
